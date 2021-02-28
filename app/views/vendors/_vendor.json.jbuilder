e076edcb46619d163d716a8b6fa7d7d2a633e44d
dneighbors <dneighbo@gmail.com>
2021-02-28 16:06:54 -0600

    add vendors CRUD

---
+++ app/views/vendors/_vendor.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! vendor, :id, :name, :website_url, :created_at, :updated_at
+json.url vendor_url(vendor, format: :json)
---
+++ db/migrate/20210228220639_create_vendors.rb
@@ -0,0 +1,10 @@
+class CreateVendors < ActiveRecord::Migration[6.1]
+  def change
+    create_table :vendors do |t|
+      t.string :name
+      t.string :website_url
+
+      t.timestamps
+    end
+  end
+end
